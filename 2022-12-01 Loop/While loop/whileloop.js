014342bc0218c1ba0d2e259e69cfba71e5fb5bba
Evening <evening@lkhagva-erdenes-iMac.local>
2022-12-01 19:12:41 +0800

    loop edited

--- 2022-12-01 Loop/While loop/whileloop.js
+++ 2022-12-01 Loop/While loop/whileloop.js
@@ -1,3 +1,44 @@
+1.
+console.log('1-10 hurtelh too hevleh program')
 for(let count = 1; count <= 10; count ++){
     console.log(count)
 }
+2.
+console.log('10 hurtelh natural toonii sondgoi toog hevlene')
+let num = 1
+while(num < 10){
+    console.log(num)
+    num += 2
+}
+3.
+console.log('10 hurtelh natural toony tegsh too hevlene')
+let sum = 0
+while(sum < 10){
+    sum += 2
+    console.log(sum)
+}
+4.
+console.log('100 hurtelh natural toonyy niilber olno.')
+let ultra = 100 
+let a = 0
+let b = 0
+while (a < ultra) {
+    a ++
+    b = b + a 
+}
+console.log(b)
+5.
+console.log('N hurtel natural toony jagsaalt gargan niilberiig olno')
+let n = Number(prompt('Duriin natural toogoo bichne uu'))
+for(let counter = 1; counter <= n; counter ++){
+    console.log(counter)
+}if (a < n){
+     a ++
+    b = b + a 
+}
+console.log(b)
+
+6.
+console.log('Ogogdson too anhny too mon esehiig shalgana')
+let i = Number(prompt('Anhny too mon esehiig shalgamaar baigaa toogoo bichne uu'))
+
---
+++ 2022-12-01 Loop/toonii niilber/toonii.js
@@ -0,0 +1,10 @@
+// let num = Number(prompt('Duriin neg toogoo oruulna uu ?'))
+// alert((num*(num+1))/2)
+let num = Number(prompt("Ta duriin toogo oruulna uu"));
+let a = 0;
+let b = 0;
+while(a < num){
+    a = a+1;
+    b = a+b;
+}
+alert(b)
