05541da8e9259036c82ab434188a68f38d8e4fd6
Amka <amka@Amkas-MacBook-Pro.local>
2022-12-01 13:53:19 +0800

    simple

---
+++ 2022-11-30 Number&String/String-simple assignment/script.js
@@ -0,0 +1,21 @@
+1.// ner harithuulah 
+const fname = 'Amartuvshin'
+const lname = 'Batdelger'
+console.log(fname.length,lname.length)
+2,3.// tom jijgeer gargah 
+console.log(lname.toUpperCase())
+console.log(fname.toLowerCase())
+4.// neg mor bolgon negtgeh
+const pine = 'Pinecone academy- н' 
+const leap = 'leap хөтөлбөрт тавтай морилно уу?'
+console.log(pine.concat(' ',leap))
+
+5.//
+let firstName = 'Nyambaatar gedeg.'
+let lastName = 'Minii neriig Galbadrakh ovogtoi'
+let country = 'Bi Mongol ulsad torson.'
+let city = 'Odoogoor Ulaanbaatar hotod amidardag'
+let age = '23 nastai.'
+let job = 'Minii mergejil software engineer.'
+console.log(lastName.concat(' ',firstName,country,city,' ',age,job))
+6. // 
