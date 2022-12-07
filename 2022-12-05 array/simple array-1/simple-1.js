65128f9b301266746d9a3d63cdb4d14db2159cf4
Evening <evening@lkhagva-erdenes-iMac.local>
2022-12-07 19:10:02 +0800

    method

---
+++ 2022-12-05 array/simple array-1/simple-1.js
@@ -0,0 +1,19 @@
+
+let itCompanies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+1.
+console.log(itCompanies)
+2.
+console.log(itCompanies.length)
+3.
+for(let i = 0; i < itCompanies.length; i += 3){
+    console.log(itCompanies[i])
+}
+4.
+console.log('endees buh company hevlene')
+for(let j = 0; j < itCompanies.length; j ++){
+    console.log(itCompanies[j])
+}
+5.
+for(let x = 0; x < itCompanies.length; x ++){
+
+}
---
+++ 2022-12-07 Methods/method.js
@@ -0,0 +1,22 @@
+let n = Number(prompt( 'gurvalhin uusgeh toon ogogdloo oruulna uu'));
+let result = '';
+for(let i = 0; i < n; i ++){   
+    for( j = 0; j < n - i - 1 ; j ++){
+        result = result + ' '
+    }
+    if (i === 0 || i === n - 1){
+        for(let j = 0; j < i + 1; j ++){
+            result = result + '* '
+        }
+    }else {
+        for( let j = 0; j < i + 1; j ++){
+            if(j === 0 || j === i){
+                result = result + '* ';
+            }else{
+                result = result +'  ';
+            }
+        }
+    }
+    result = result + '\n'
+}
+console.log(result)
